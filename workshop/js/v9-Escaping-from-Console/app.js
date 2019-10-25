7e6fb966d7b16ad32bbf645c5d28fef04df0d54a
Gökhan Sincar <55689973+gokhansincar@users.noreply.github.com>
2019-10-25 13:42:45 +0200

    update

--- workshop/js/v9-Escaping-from-Console/app.js
+++ workshop/js/v9-Escaping-from-Console/app.js
@@ -6,38 +6,24 @@ let todos = {
   //MY TODO LIST - PROPERTY
   list : [
 
-    {
-      text: "Learn HTML5",
-      completed: true
-    },
-    {
-      text: "Learn CSS",
-      completed: true
-    },
-    {
-      text: "Learn JS",
-      completed: false
-    },
-    {
-      text: "Learn PHP",
-      completed: false
-    }
+    // {
+    //   text: "Learn HTML5",
+    //   completed: true
+    // },
+    // {
+    //   text: "Learn CSS",
+    //   completed: true
+    // },
+    // {
+    //   text: "Learn JS",
+    //   completed: false
+    // },
+    // {
+    //   text: "Learn PHP",
+    //   completed: false
+    // }
 
   ],
-
-  //DISPLAY TODOS - METHOD
-  displayTodos: function() {
-
-    if(this.list.length == 0) {
-      console.log("You don't have any todos, add some!");
-    }
-
-    this.list.forEach(function(item) {
-      let completedStr = (item.completed) ? "(x)" : "( )";
-      console.log(completedStr, item.text);
-    });
-    console.log("-------------");
-  },
 
   //ADD TODO
   addTodo: function(todoText) {
@@ -48,19 +34,19 @@ let todos = {
     }
 
     this.list.push(newTodo);
-    this.displayTodos();
+    view.displayTodos();
   },
 
   //CHANGE TODO
   changeTodo: function(index, newText) {
     this.list[index].text = newText;
-    this.displayTodos();
+    view.displayTodos();
   },
 
   //DELETE TODO
   deleteTodo: function(index) {
     this.list.splice(index, 1);
-    this.displayTodos();
+    view.displayTodos();
   },
 
   //TOGGLE COMPLETED
@@ -68,7 +54,7 @@ let todos = {
   
     let currentStatus = this.list[index].completed; //true or false
     this.list[index].completed = ! currentStatus;
-    this.displayTodos();
+    view.displayTodos();
 
   },
 
@@ -102,7 +88,7 @@ let todos = {
       });
     }
 
-    this.displayTodos();
+    view.displayTodos();
 
   }
 
@@ -118,8 +104,13 @@ let handlers = {
   //Check if an input is empty. Alert if so
   isEmpty: function(input) {
 
+    const msgElem = document.querySelector("#msg");
+    const msgElemText = document.querySelector("#msgText");
+
     if(input.value === '') {
-      alert("The input ''" + input.id + "'' cannot be empty!");
+      // alert("The input ''" + input.id + "'' cannot be empty!");
+      msgElemText.innerHTML = "The input ''" + input.id + "'' cannot be empty!";
+      msgElem.classList.remove('hidden');
       return true;
     }
     else {
@@ -176,86 +167,63 @@ let handlers = {
 };
 
 
+/* VIEW OBJECT
+ * Specialty: create and display HTML
+-----------------------------------------------*/
+let view = {
 
-//LINK YOUR HTML buttons
-// const btnDisplay = document.getElementById('btnDisplay');
-// const btnToggleAll = document.getElementById('btnToggleAll');
+  //DISPLAY TODOS - METHOD
+  displayTodos: function() {
 
-//Display todos btn
-// btnDisplay.addEventListener('click', function() {
-//   todos.displayTodos();
-// });
+    const ul = document.querySelector("#todo-list");
+    const msgElem = document.querySelector("#msg");
+    const msgElemText = document.querySelector("#msgText");
+    ul.innerHTML = '';
 
-//Toggle todos btn
-// btnToggleAll.addEventListener('click', function() {
-//   todos.toggleAll();
-// });
+    if(todos.list.length === 0) {
 
-//ADD Todo btn and input
-// const btnAdd = document.getElementById('btnAdd');
+      msgElemText.innerHTML = "Your list is empty, please add something.";
+      
+      // msgElem.setAttribute('class', '');
+      // msgElem.removeAttribute('class');
+      msgElem.classList.remove('hidden');
 
-// btnAdd.addEventListener('click', function() {
-
-//   const inputAdd = document.getElementById('inputAdd');
-
-//   if(inputAdd.value !== '') {
-//     todos.addTodo(inputAdd.value);
-//     inputAdd.value = '';
-//   }
-//   else {
-//     alert("The input text cannot be empty!");
-//   }
-
-// });
-
-//TOGGLE Todo btn and input
-// const btnToggle = document.getElementById('btnToggle');
-
-// btnToggle.addEventListener('click', function() {
-
-//   const inputToggleIndex = document.getElementById('inputToggleIndex');
-
-//   if(inputToggleIndex.value !== '') {
-//     todos.toggleTodo(inputToggleIndex.value);
-//     inputToggleIndex.value = '';
-//   }
-//   else {
-//     alert("The index cannot be empty!");
-//   }
-
-// });
+    }
+    else {
+      msgElem.classList.add('hidden');
+    }
 
 
+    //LOOP INSIDE YOUR TODO LIST
+    todos.list.forEach(function(item) {
+
+      let li = document.createElement('li');
+      let completedStr = (item.completed) ? "(x)" : "( )";
+      li.innerHTML = completedStr + "&nbsp;&nbsp;" + item.text;
+      
+      ul.appendChild(li);
+      
+    });
+    
+  }
+
+};
+
+view.displayTodos();
 
 
-// todos.addTodo("asdasd");
+//CREATE AN HTML ELEMENT
+// let div = document.createElement("div");
+// div.innerHTML = 'Something';
+// div.setAttribute('class', 'row');
 
+// // document.body.insertBefore(div, null);
+// document.body.appendChild(div);
 
+//ADD AN LI TO MAY UL LIST
+// let ul = document.getElementById("todo-list");
+// let ul = document.querySelector('ul');
 
-
-//IN jQuery is something like this:
-// $("#btnDisplay").on('click', function() {
-//   todos.displayTodos();
-// })
-
-//WHAT WE'VE MISSED :
-//1. STRICT COMPARISON
-/*
-let num = "3";
-if(num === 3) {
-  console.log("TRUE");
-}
-else {
-  console.log("FALSE");
-}
-let bool = 1;
-if(bool === true) {
-  console.log("TRUE");
-}
-else {
-  console.log("FALSE");
-}
-//2. You can write conditions like this :
-if(true) console.log("TRUE");
-else console.log("FALSE");
-*/
+// let li = document.createElement("li");
+// li.innerHTML = '(x) Learn HTML';
+// ul.appendChild(li);
